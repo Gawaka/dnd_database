822d0d4e26548e271baf2b231b4e20a136055897
Gawaka <paskm001@gmail.com>
2026-01-28 21:29:27 +0200

    update-header/navigate

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,20 @@ export function Header() {
 
     return(
     <header className='header'>
-        <img src={logo} alt="logo" className='header__logo'/>
-        <h1 className='header__title'>characters database</h1>
+        <div className="header__title-wrapper">
+            <img src={logo} alt="logo" className='header__logo'/>
+            <h1 className='header__title'>Dungeons & Dragons database</h1>
+        </div>
+        <div className="header__navigate-wrapper">
+            <nav className='header__navigate'>
+                <ul className="header__navigate-items">
+                    <li className="header__navigate-item">Home</li>
+                    <li className="header__navigate-item">Equipment</li>
+                    <li className="header__navigate-item">Scenarios</li>
+                    <li className="header__navigate-item">About</li>
+                </ul>
+            </nav>
+        </div>
     </header>
     )
 }
